a0ce652b3ebc1a10a234340bd8a268c3d5f4344a
NeilChainani <neilchainani1@gmail.com>
2015-05-03 19:47:30 -0700

    ruruonline site layout

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+
+<header>
+<meta http-equiv="Content-Language" content="en"> 
+<meta charset="UTF-8"> 
+<meta name="description" content="Looking for events in your area?  Ruruonline is your  guide for top events, tickets &amp; more!">
+<link rel="stylesheet"  type="text/css" href = "rstyles/styles.css" >
+
+</header>
+
+<body>
+<div id = "rheaderwrap">
+
+
+
+<div id = "rheader">
+<div id = "rlogo">
+<a href="http://ruruonline.com"><img src="rimages/logo.png"></a>
+</div>
+<div id = "ractions">
+<ul>
+<li>
+<a href = "javascript:void(0);"><span>Login/Sign up</span></a>
+</li>
+</ul>
+</div>
+</div>
+</div>	
+<div id="rnavwrap">
+<div id="rnav">
+<ul id="rnavbar">
+			<li>
+				<a href="http://ruruonline.com/"> <span>Home</span></a>
+			</li>
+            			<li>
+				<a href="http://ruruonline.com/Events/"><span>Events</span></a>
+			</li>
+            							<li>
+					<a href="http://ruruonline.com/forums/"><span>Forums</span></a>
+				</li>
+			            				<li>
+					<a href="http://ruruonline.com/blog/"><span>Blog</span></a>
+				</li>
+									                                     
+		</ul>
+</div>
+
+</div>
+</div>
+
+
+</body>
+
+
+</html>
